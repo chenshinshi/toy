53a232df63b11fc4930528e5a815d6e0a9c06284
chenshinshi <125173405+chenshinshi@users.noreply.github.com>
2023-06-27 12:12:24 +0800

    1.1.7 上传文件

---
+++ README.md
@@ -0,0 +1,26 @@
+# Toy 思源笔记主题
+
+![preview](https://raw.githubusercontent.com/langzhou/toy-theme-for-siyuan/main/preview.png)
+
+这是一款「玩具」主题，将页面布局和配色分离，通过简单的配置即可快速实现多种主题样式效果。
+
+目前支持的配置：
+- 独立的配色文件
+- 全屏下自动隐藏顶部工具栏
+- 设置左边栏背景色
+- 设置左边栏背景图
+- 全局毛玻璃效果
+- 多种页签栏样式
+
+### 使用方法
+将`theme.css`中的`@import url("palette/XXX.css")`中的XXX.css 替换成其他的配色文件，目前已提供以下配色文件：
+- yinxiang.css：仿照印象笔记配色，会自动隐藏顶栏
+- blue.css：蓝色暗调
+- cyan.css：兰色暗调
+- light.css：亮色配色模板，可修改此文件创建自己的配色方案
+- dark.css：深色配色模板，可修改此文件创建自己的配色方案
+- green.css：护眼主题
+- 四季主题：spring.css、autumn.css、winter.css
+
+### 已知问题
+- 主题迭代器不支持独立窗口
---
+++ theme.js
@@ -0,0 +1,151 @@
+// REF https://github.com/Zuoqiu-Yingyi/siyuan-theme-dark-plus/blob/main/theme.js
+window.theme = {};
+
+/* 颜色配置文件列表 */
+window.theme.colors = [
+    'palette/autumn.css',
+    'palette/blue.css',
+    'palette/cyan.css',
+    'palette/dark.css',
+    'palette/green.css',
+    'palette/light.css',
+    'palette/spring.css',
+    'palette/tonight.css',
+    'palette/winter.css',
+    'palette/yinxiang.css',
+];
+
+/* DOM 节点 ID */
+window.theme.IDs = {
+    STYLE_COLOR: 'custom-id-style-theme-color',
+    BUTTON_TOOLBAR_CHANGE_COLOR: 'custom-id-button-toolbar-change-color',
+    LOCAL_STORAGE_COLOR_HREF: 'langzhou-color-href',
+};
+
+/* 循环迭代器 */
+window.theme.Iterator = function* (items) {
+    // REF [ES6中的迭代器(Iterator)和生成器(Generator) - 小火柴的蓝色理想 - 博客园](https://www.cnblogs.com/xiaohuochai/p/7253466.html)
+    for (let i = 0; true; i = (i + 1) % items.length) {
+        yield items[i];
+    }
+}
+
+/**
+ * 加载样式文件
+ * @params {string} href 样式地址
+ * @params {string} id 样式 ID
+ */
+window.theme.loadStyle = function (href, id = null) {
+    let style = document.createElement('link');
+    if (id) style.id = id;
+    style.type = 'text/css';
+    style.rel = 'stylesheet';
+    style.href = href;
+    document.head.appendChild(style);
+}
+
+/**
+ * 更新样式文件
+ * @params {string} id 样式文件 ID
+ * @params {string} href 样式文件地址
+ */
+window.theme.updateStyle = function (id, href) {
+    let style = document.getElementById(id);
+    if (style) {
+        style.setAttribute('href', href);
+    }
+    else {
+        window.theme.loadStyle(href, id);
+    }
+}
+
+setTimeout(() => {
+    const drag = document.getElementById('drag'); // 标题栏
+    const themeStyle = document.getElementById('themeStyle'); // 当前主题引用路径
+    if (drag && themeStyle) {
+        const THEME_ROOT = new URL(themeStyle.href).pathname.replace('theme.css', ''); // 当前主题根目录
+        /* 通过颜色配置文件列表生成完整 URL 路径 */
+        const colors_href = [];
+        window.theme.colors.forEach(color => colors_href.push(`${THEME_ROOT}${color}`));
+        window.theme.iter = window.theme.Iterator(colors_href);
+        var color_href = window.siyuan?.storage[window.theme.IDs.LOCAL_STORAGE_COLOR_HREF];
+        if (!color_href) {
+            localStorage.getItem(window.theme.IDs.LOCAL_STORAGE_COLOR_HREF);
+        }
+        if (color_href) { // 将迭代器调整为当前配色
+            for (let i = 0; i < window.theme.colors.length; ++i) {
+                if (window.theme.iter.next().value === color_href) break;
+            }
+        }
+        else { // 迭代器第一个为当前配色
+            color_href = window.theme.iter.next().value;
+            localStorage.setItem(window.theme.IDs.LOCAL_STORAGE_COLOR_HREF, color_href);
+            setLocalStorageVal(window.theme.IDs.LOCAL_STORAGE_COLOR_HREF, color_href);
+        }
+
+        /* 加载配色文件 */
+        window.theme.updateStyle(window.theme.IDs.STYLE_COLOR, color_href);
+
+        const button_change_color = document.createElement('button'); // 切换主题颜色按钮
+        button_change_color.id = window.theme.IDs.BUTTON_TOOLBAR_CHANGE_COLOR;
+        button_change_color.className = 'toolbar__item b3-tooltips b3-tooltips__sw';
+        button_change_color.ariaLabel = '切换主题颜色';
+        button_change_color.innerHTML = `<svg><use xlink:href="#iconTheme"></use></svg>`;
+        button_change_color.addEventListener('click', e => {
+            color_href = window.theme.iter.next().value;
+            localStorage.setItem(window.theme.IDs.LOCAL_STORAGE_COLOR_HREF, color_href);
+            setLocalStorageVal(window.theme.IDs.LOCAL_STORAGE_COLOR_HREF, color_href);
+            window.theme.updateStyle(window.theme.IDs.STYLE_COLOR, color_href);
+        });
+        // REF [JS DOM 编程复习笔记 -- insertAdjacentHTML（九） - 知乎](https://zhuanlan.zhihu.com/p/425616377)
+        drag.insertAdjacentElement('afterend', button_change_color);
+        drag.insertAdjacentHTML('afterend', `<div class="protyle-toolbar__divider"></div>`);
+
+    }
+}, 0);
+
+/**
+ * 发送API请求
+ * @param {*} data 
+ * @param {*} url 
+ * @returns 
+ */
+async function postRequest(data, url){
+    let result;
+    await fetch(url, {
+        body: JSON.stringify(data),
+        method: 'POST',
+        headers: {
+            "Authorization": "Token ",
+            "Content-Type": "application/json"
+        }
+    }).then((response) => {
+        result = response.json();
+    });
+    return result;
+}
+/**
+ * 设置LocalStorage
+ * @param {*} ikey 
+ * @param {*} ival 
+ */
+async function setLocalStorageVal(ikey, ival) {
+    let url = "/api/storage/setLocalStorageVal";
+    let response = await postRequest({app: getAppId(), key: ikey, val: ival}, url);
+    if (window.top.siyuan.storage != undefined) {
+        window.top.siyuan.storage[ikey] = ival;
+    }
+    function getAppId() {
+        let wsurl = window.top.siyuan.ws.ws.url;
+        let appIdMatchResult = wsurl.match(new RegExp(`(\\?app=|&app=)[^&]+`, "g"));
+        if (appIdMatchResult.length == 1){
+            return appIdMatchResult[0].substring(5);
+        }else if (appIdMatchResult.length > 1) {
+            console.warn("正则获取appId错误", appIdMatchResult);
+            return appIdMatchResult[0].substring(5);
+        }else {
+            console.error("正则获取appId错误", appIdMatchResult);
+            return "";
+        }
+    }
+}
